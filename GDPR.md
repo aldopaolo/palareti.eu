a6e429d422778f96e7dfe40c1959204c6a7b4692
aldopaolo <aldopaolo@palareti.eu>
2022-08-22 21:00:47 +0200

    Modifica della gestione delle fonti

---
+++ GDPR.md
@@ -0,0 +1,11 @@
+# Compliance GDPR
+
+## compliance CDN Unpkg
+
+> Unpkg does not collect, process, or store any personal information, including client IPs, so it is not subject to GDPR.
+
+[@mjackson in risposta a @chrtzvonbrtz](<https://twitter.com/mjackson/status/987342097773617152?ref_src=twsrc%5Etfw%7Ctwcamp%5Etweetembed%7Ctwterm%5E987342097773617152%7Ctwgr%5E8d3f90cbc47800df35a215ba058e227fe962cd12%7Ctwcon%5Es1_&ref_url=https%3A%2F%2Fwordpress.org%2Fsupport%2Ftopic%2Fgdpr-compliance-296%2F>)
+
+## compliance Open Street Map
+
+Vedi [Preparing for the GDPR](<https://blog.openstreetmap.org/2018/05/14/preparing-for-the-gdpr/>)
---
+++ aid/palareti.js
@@ -0,0 +1,53 @@
+﻿
+(function() {
+  /*
+    cambia le varibili globali usate in bootstrap
+    da https://www.w3schools.com/css/css3_variables_javascript.asp
+  */
+  /*
+    spiegazione di alcuni nomi che possono apparire in font-family
+    -apple-system:
+      San Francisco in Safari(on Mac OS X and iOS); Neue Helvetica and Lucida Grande
+      on older versions of Mac OS X.
+      It properly selects between San Francisco Text and San Francisco Display depending
+      on the text’s size.
+    system-ui:
+      default UI font on a given platform.
+    BlinkMacSystemFont:
+      equivalent of-apple-system, for Chrome on Mac OS X.
+    "Segoe UI":
+      Windows(Vista+) and Windows Phone.
+    Roboto:
+      Android(Ice Cream Sandwich(4.0)+) and Chrome OS.
+      It is deliberately listed after Segoe UI so that if you’re an Android developer on
+      Windows and have Roboto installed, Segoe UI will be used instead
+    Ubuntu:
+      all versions of Ubuntu.
+
+    fonti:
+    https://github.com/necolas/normalize.css/issues/665
+    https://stackoverflow.com/questions/32660748/how-to-use-apples-new-san-francisco-font-on-a-webpage
+  */
+  const root=document.querySelector(':root');
+  root.style.setProperty(
+    '--bs-font-sans-serif',
+    `"Titilium Web", system-ui, -apple-system,
+    "Segoe UI", Roboto, "Helvetica Neue", "Noto Sans", "Liberation Sans", Arial, sans-serif,
+    "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`
+  );
+  /*
+  in bootstrap era:
+    system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", "Noto Sans",
+    "Liberation Sans", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+    "Segoe UI Symbol", "Noto Color Emoji";
+  */
+  root.style.setProperty(
+    '--bs-font-monospace',
+    `"Roboto Mono", SFMono-Regular, Menlo, Monaco, Consolas,
+    "Liberation Mono", "Courier New", monospace`
+  );
+  /*
+    in bootstrap era:
+    SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+  */
+})();
