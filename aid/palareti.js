7bf17810ac2770f137f61dcf2bfdef353ecb2a04
aldopaolo <aldopaolo@palareti.eu>
2022-08-22 21:04:30 +0200

    Generalizzazione gestione file JSON

--- aid/palareti.js
+++ aid/palareti.js
@@ -1,4 +1,5 @@
-﻿
+﻿'use strict'
+// ================ modifiche di bootstrap ==================
 (function() {
   /*
     cambia le varibili globali usate in bootstrap
@@ -51,3 +52,34 @@
     SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
   */
 })();
+
+// ================ funzione generica per acqisire un file JSON locale ==================
+function getJson(fileUrl) {
+  let xhr=new XMLHttpRequest();
+  let response="[]";
+  let ret;
+
+  // acquisico il file nella variabile response
+  xhr.open('GET', fileUrl, false); // false: chiamata asincrona
+  xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  try {
+    xhr.send();
+    if(xhr.status===200) {
+      response=xhr.response;
+    } else {
+      alert(`Error in file ${fileUrl}: Status ${xhr.status}, "${xhr.statusText}"`);
+    }
+  } catch(err) { // instead of onerror
+    alert(`Request failed -- file ${fileUrl}`);
+  }
+
+  // estraggo il formato Json nella variabile ret
+  try {
+    ret=JSON.parse(response);
+  } catch(err) {
+    alert(`Errori nel file json -- file ${fileUrl}`);
+    ret=[];
+  }
+
+  return ret;
+}
